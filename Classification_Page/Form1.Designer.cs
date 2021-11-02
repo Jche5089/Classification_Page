dde179327ad56d9d8548d17d6b11bb36075d9947
정철 심 <wjatn5089@gmail.com>
2021-11-02 09:36:33 +0900

    초기 커밋.

---
+++ Classification_Page/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿
+namespace Classification_Page
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.GridRecord = new System.Windows.Forms.DataGridView();
+            this.코드 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.명칭 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.비고 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridRecord)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(4, 69);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 129);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 10F);
+            this.label4.Location = new System.Drawing.Point(16, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 14);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "비고";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 94);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 21);
+            this.textBox4.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 10F);
+            this.label3.Location = new System.Drawing.Point(17, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 14);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "명칭";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 22);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 21);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 10F);
+            this.label2.Location = new System.Drawing.Point(17, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 14);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "코드";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 57);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 21);
+            this.textBox3.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button10);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Location = new System.Drawing.Point(2, 16);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1115, 45);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "분류코드 등록";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "거래처유형";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "거래처지역";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(649, 11);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "상품유형";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "배송담당";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(455, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "단가구분";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1008, 11);
+            this.button10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(63, 23);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "닫기";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.close_btn);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(552, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "제조사";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(947, 11);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "삭제";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.delete_btn);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(826, 11);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "신규";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.new_btn);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(886, 11);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "저장";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.save_btn);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.GridRecord);
+            this.panel3.Controls.Add(this.button11);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Location = new System.Drawing.Point(765, 73);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(349, 222);
+            this.panel3.TabIndex = 2;
+            // 
+            // GridRecord
+            // 
+            this.GridRecord.AllowUserToAddRows = false;
+            this.GridRecord.AllowUserToDeleteRows = false;
+            this.GridRecord.AllowUserToOrderColumns = true;
+            this.GridRecord.AllowUserToResizeRows = false;
+            this.GridRecord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.GridRecord.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.GridRecord.BackgroundColor = System.Drawing.Color.White;
+            this.GridRecord.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GridRecord.ColumnHeadersHeight = 30;
+            this.GridRecord.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.코드,
+            this.명칭,
+            this.비고});
+            this.GridRecord.EnableHeadersVisualStyles = false;
+            this.GridRecord.GridColor = System.Drawing.Color.PowderBlue;
+            this.GridRecord.Location = new System.Drawing.Point(26, 62);
+            this.GridRecord.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GridRecord.Name = "GridRecord";
+            this.GridRecord.ReadOnly = true;
+            this.GridRecord.RowHeadersVisible = false;
+            this.GridRecord.RowHeadersWidth = 51;
+            this.GridRecord.RowTemplate.Height = 27;
+            this.GridRecord.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.GridRecord.Size = new System.Drawing.Size(309, 142);
+            this.GridRecord.TabIndex = 2;
+            // 
+            // 코드
+            // 
+            this.코드.HeaderText = "코드";
+            this.코드.MinimumWidth = 6;
+            this.코드.Name = "코드";
+            this.코드.ReadOnly = true;
+            // 
+            // 명칭
+            // 
+            this.명칭.HeaderText = "명칭";
+            this.명칭.MinimumWidth = 6;
+            this.명칭.Name = "명칭";
+            this.명칭.ReadOnly = true;
+            // 
+            // 비고
+            // 
+            this.비고.HeaderText = "비고";
+            this.비고.MinimumWidth = 6;
+            this.비고.Name = "비고";
+            this.비고.ReadOnly = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(213, 13);
+            this.button11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(63, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "검색";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.search_btn);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 21);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 625);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridRecord)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView GridRecord;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 코드;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 명칭;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 비고;
+    }
+}
+
---
+++ Classification_Page/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classification_Page
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindData(string condition)
+        {
+            if (this.s구분 == "3" || this.s구분 == "4")
+            {
+                this.bindData2(condition);
+            }
+            else
+            {
+                this.GridRecord.RowCount = 0;
+                this.GridRecord.DataSource = (object)null;
+                try
+                {
+                    DataTable dataTable = new wnDm().fn_분류코드_List(this.s테이블명, condition, Common.p_strConn);
+                    if (dataTable != null && dataTable.Rows.Count > 0)
+                    {
+                        this.GridRecord.RowCount = dataTable.Rows.Count;
+                        for (int index = 0; index < dataTable.Rows.Count; ++index)
+                        {
+                            this.GridRecord.Rows[index].Cells[0].Value = (object)dataTable.Rows[index]["코드"].ToString();
+                            this.GridRecord.Rows[index].Cells[1].Value = (object)dataTable.Rows[index]["명칭"].ToString();
+                            this.GridRecord.Rows[index].Cells[2].Value = (object)dataTable.Rows[index]["비고"].ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    wnLog.writeLog(100, ex.Message + " - " + ex.ToString());
+                }
+            }
+        }
+
+        private void bindData2(string condition)
+        {
+            this.GridRecord.RowCount = 0;
+            this.GridRecord.DataSource = (object)null;
+            try
+            {
+                DataTable dataTable = new wnDm().fn_분류코드2_List(this.s테이블명, condition, Common.p_strConn);
+                if (dataTable == null || dataTable.Rows.Count <= 0)
+                    return;
+                this.GridRecord.RowCount = dataTable.Rows.Count;
+                for (int index = 0; index < dataTable.Rows.Count; ++index)
+                {
+                    this.GridRecord.Rows[index].Cells[0].Value = (object)dataTable.Rows[index]["코드"].ToString();
+                    this.GridRecord.Rows[index].Cells[1].Value = (object)dataTable.Rows[index]["명칭"].ToString();
+                    this.GridRecord.Rows[index].Cells[2].Value = (object)dataTable.Rows[index]["비고"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                wnLog.writeLog(100, ex.Message + " - " + ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void new_btn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_btn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_btn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void close_btn(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_btn(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
